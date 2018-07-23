fe2e4167d7a8ca51d7cb5e882e37711f7e1dde00
Caio <caiobrunos@hotmail.com>
2018-07-22 21:49:40 -0300

    Commit inicial

---
+++ src/Crime.java
@@ -0,0 +1,84 @@
+
+public class Crime {
+	private int dia;
+	private int mês;
+	private int ano;
+	private int sexo;
+	private int tArma;
+	private int idade;
+	private int cvli;
+	private double latitude;
+	private double longitude;
+	public Crime(int dia, int mês, int ano, int sexo, int tArma, int idade, int cvli, double latitude, double longitude) {
+		super();
+		this.setDia(dia);
+		this.mês = mês;
+		this.ano = ano;
+		this.sexo = sexo;
+		this.tArma = tArma;
+		this.idade = idade;
+		this.cvli = cvli;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public int getMês() {
+		return mês;
+	}
+	public void setMês(int mês) {
+		this.mês = mês;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int gettArma() {
+		return tArma;
+	}
+	public void settArma(int tArma) {
+		this.tArma = tArma;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public int getCvli() {
+		return cvli;
+	}
+	public void setCvli(int cvli) {
+		this.cvli = cvli;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public int getDia() {
+		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(int sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+}
---
+++ src/CrimeRepo.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class CrimeRepo implements Serializable{
+	
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8635603547323350634L;
+
+private static CrimeRepo instance;
+	
+	private ArrayList<Crime> crimes = new ArrayList<>();
+	private static String path = "C:\\Users\\Caio\\workspace\\Reznov\\src"; 
+	
+	public static CrimeRepo getInstance(){
+		if(instance == null){
+			instance = lerDoArquivo();
+		}
+		return instance;
+	}
+	
+	private CrimeRepo(){
+	}
+	
+	private static CrimeRepo lerDoArquivo(){
+		CrimeRepo instanciaLocal = new CrimeRepo();
+		try {
+
+			FileReader arq = new FileReader(path);
+			BufferedReader lerArq = new BufferedReader(arq);
+			String linha;
+			String[] valores;
+			Crime atual;
+			
+			
+			while ((linha = lerArq.readLine()) != null) {
+				linha = lerArq.readLine();
+				valores = linha.split(",");
+				atual = new Crime(Integer.parseInt(valores[0]),
+									Integer.parseInt(valores[1]),
+									Integer.parseInt(valores[2]),
+									Integer.parseInt(valores[3]),
+									Integer.parseInt(valores[4]),
+									Integer.parseInt(valores[5]),
+									Integer.parseInt(valores[6]),
+									Double.parseDouble(valores[7]),
+									Double.parseDouble(valores[8])
+									);
+				instanciaLocal.cadastrar(atual);
+				
+			}
+			
+			arq.close();
+		}catch(Exception e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+		}
+		return instanciaLocal;
+	}
+	
+	public void salvarArquivo(){
+		
+		File out = new File("curso.dat");
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		
+		try{
+			fos = new FileOutputStream(out);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(instance);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(oos!=null){
+				try{
+					oos.close();
+				}
+				catch(Exception e){
+//					não faz nada
+				}
+			}
+		}
+	}
+	
+	public void cadastrar(Crime c){
+		this.crimes.add(c);
+	}
+	
+}
+
